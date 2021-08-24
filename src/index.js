5ffe75c0ef0846c1d0447e218ffff0d6128227b1
오종택 <jongtaek@ojongtaeg-ui-MacBookPro.local>
2021-08-24 10:46:00 +0900

    delete: dist/

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
-ReactDom.render(<App page="home" />, document.getElementById("root"));
+// ReactDom.render(<App page="home" />, document.getElementById("root"));
 
 const initialData = window.__INITIAL_DATA__;
 ReactDom.hydrate(
-  <App page={initialData.page} />,
+  <App page={initialData?.page} />,
   document.getElementById("root")
 );
